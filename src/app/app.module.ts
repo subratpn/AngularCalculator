0e0e7e60b2f475f9b2dbe008a625ff66b2f36f45
Subrat Pani <subratpn@gmail.com>
2019-12-03 15:56:27 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { ContentComponent } from './content/content.component';
+import { ModalBodyComponent } from './modal-body/modal-body.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SidenavComponent,
+    ContentComponent,
+    ModalBodyComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modal-body/modal-body.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-body',
+  templateUrl: './modal-body.component.html',
+  styleUrls: ['./modal-body.component.css']
+})
+export class ModalBodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
